b09b13e9776a14272bb165a18afafdb58bc0ba80
rizsyad <rizsyad@gmail.com>
2022-04-13 20:04:21 +0700

    add a monthly prayer schedule

--- pages/api/prayer/[city].js
+++ pages/api/prayer/[city].js
@@ -1,41 +1,43 @@
 import axios from "axios";
+
 export default async function handler(req, res) {
   const { city } = req.query;
 
-  let json = { code: 200, status: "OK", result: null };
-
   if (!city)
     return res.status(404).json({
-      result: "city is required",
       code: 404,
       status: "error",
+      result: "city is required",
     });
 
   if (req.method !== "GET")
     return res.status(405).json({
-      result: "city is required",
       code: 405,
       status: "error",
+      result: "Method Not Allowed",
     });
 
-  const date = new Date().toISOString().substring(0, 10);
+  const date = new Date().toISOString().substring(0, 10).split("-");
+  const getYear = date[0];
+  const getMonth = date[1];
 
   try {
     const { data } = await axios(
-      `https://api.pray.zone/v2/times/day.json?city=${city}&date=${date}`
+      `https://api.myquran.com/v1/sholat/jadwal/${city}/${getYear}/${getMonth}`
     );
 
-    const { times } = data.results.datetime[0];
+    const { jadwal } = data.data;
 
     res.status(200).json({
-      ...json,
-      result: times,
+      code: 200,
+      status: "OK",
+      result: jadwal,
     });
   } catch (error) {
     return res.status(404).json({
-      result: "data not found",
       code: 404,
       status: "error",
+      result: "Data not found",
     });
   }
 }
---
+++ pages/api/prayer/city.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET")
+    return res.status(405).json({
+      code: 405,
+      status: "error",
+      result: "Method Not Allowed",
+    });
+
+  const { data } = await axios(`https://api.myquran.com/v1/sholat/kota/semua`);
+
+  res.status(200).json({
+    code: 200,
+    status: "OK",
+    result: data,
+  });
+}
