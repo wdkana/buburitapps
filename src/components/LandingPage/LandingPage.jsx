d5da361d20ff1384d07e3a54c21a8bc991a2989b
deaaprizal <dea.afrizal@icloud.com>
2022-04-13 23:46:46 +0700

    remove console

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { getCity, getPrayerTime } from "../../api/prayerAPI.js";
-import { useCountdown } from "../../hooks/useCountdown";
-import { combineDateAndTime, afterMomentHour } from "../../helpers/date.js";
+import { useEffect, useState } from "react"
+import { getCity, getPrayerTime } from "../../api/prayerAPI.js"
+import { useCountdown } from "../../hooks/useCountdown"
+import { combineDateAndTime, afterMomentHour } from "../../helpers/date.js"
 
 import {
   Container,
@@ -22,86 +22,86 @@ import {
   ProductDetailPrice,
   ProductDetailDesc,
   ProductDetailCategory,
-} from "./styled";
-import styled from "./index.module.css";
-import { checkSingleDateValue } from "../../helpers/string.js";
-import { getProduct } from "../../api/storeAPI.js";
-import Link from "next/link";
+} from "./styled"
+import styled from "./index.module.css"
+import { checkSingleDateValue } from "../../helpers/string.js"
+import { getProduct } from "../../api/storeAPI.js"
+import Link from "next/link"
 
-const LIST_CITY_ID = ["1301", "1709", "2622", "0228", "3307", "2310", "1505"];
+const LIST_CITY_ID = ["1301", "1709", "2622", "0228", "3307", "2310", "1505"]
 
 const LandingPage = () => {
-  const [selectedCity, setSelectedCity] = useState("");
-  const [listCity, setListCity] = useState([]);
-  const [date, setDate] = useState("");
-  const [isAfterMaghrib, setIsAfterMaghrib] = useState(true);
-  const [days, hours, minutes, seconds] = useCountdown(date);
-  const [listProduct, setListProduct] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("")
+  const [listCity, setListCity] = useState([])
+  const [date, setDate] = useState("")
+  const [isAfterMaghrib, setIsAfterMaghrib] = useState(true)
+  const [days, hours, minutes, seconds] = useCountdown(date)
+  const [listProduct, setListProduct] = useState([])
 
   const getTimerCountdown = () => {
-    if (isNaN(days)) return "Loading...";
+    if (isNaN(days)) return "Loading..."
 
-    return `${days}:${hours}:${minutes}:${seconds}`;
-  };
+    return `${days}:${hours}:${minutes}:${seconds}`
+  }
 
   const getTime = async () => {
     try {
-      const todayDate = new Date();
-      const todayYear = checkSingleDateValue(todayDate.getFullYear());
-      const todayMonth = checkSingleDateValue(todayDate.getMonth() + 1);
-      const todayDay = checkSingleDateValue(todayDate.getDate());
-      const selectedTodayDate = `${todayDay}/${todayMonth}/${todayYear}`;
+      const todayDate = new Date()
+      const todayYear = checkSingleDateValue(todayDate.getFullYear())
+      const todayMonth = checkSingleDateValue(todayDate.getMonth() + 1)
+      const todayDay = checkSingleDateValue(todayDate.getDate())
+      const selectedTodayDate = `${todayDay}/${todayMonth}/${todayYear}`
 
-      const response = await getPrayerTime(selectedCity, todayYear, todayMonth);
+      const response = await getPrayerTime(selectedCity, todayYear, todayMonth)
       const getSelectedResponseDate = response.find(
         (x) => x.tanggal.split(", ")[1] === selectedTodayDate
-      );
-      const { maghrib, imsak } = getSelectedResponseDate;
+      )
+      const { maghrib, imsak } = getSelectedResponseDate
 
-      const maghribTimer = combineDateAndTime(todayDate, maghrib, false);
-      const imsakTimer = combineDateAndTime(todayDate, imsak, true);
+      const maghribTimer = combineDateAndTime(todayDate, maghrib, false)
+      const imsakTimer = combineDateAndTime(todayDate, imsak, true)
 
-      const isTimeAfterMaghrib = afterMomentHour(todayDate, maghribTimer);
-      const selectedDate = isTimeAfterMaghrib ? imsakTimer : maghribTimer;
+      const isTimeAfterMaghrib = afterMomentHour(todayDate, maghribTimer)
+      const selectedDate = isTimeAfterMaghrib ? imsakTimer : maghribTimer
 
-      setIsAfterMaghrib(isAfterMaghrib);
-      setDate(selectedDate);
+      setIsAfterMaghrib(isAfterMaghrib)
+      setDate(selectedDate)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   const getListCity = async () => {
     try {
-      const response = await getCity();
+      const response = await getCity()
       const selectedListCity = response.filter((city) =>
         LIST_CITY_ID.includes(city.id)
-      );
-      setSelectedCity(selectedListCity[0].id);
-      setListCity(selectedListCity);
+      )
+      setSelectedCity(selectedListCity[0].id)
+      setListCity(selectedListCity)
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
+  }
 
   const getListProduct = async () => {
-    const products = await getProduct();
-    const sliceProducts = products.slice(0, 4);
-    setListProduct(sliceProducts);
-  };
+    const products = await getProduct()
+    const sliceProducts = products.slice(0, 4)
+    setListProduct(sliceProducts)
+  }
 
   const handleChangeCity = (e) => {
-    setSelectedCity(e.target.value);
-  };
+    setSelectedCity(e.target.value)
+  }
 
   useEffect(() => {
-    if (selectedCity) getTime();
-  }, [selectedCity]);
+    if (selectedCity) getTime()
+  }, [selectedCity])
 
   useEffect(() => {
-    getListCity();
-    getListProduct();
-  }, []);
+    getListCity()
+    getListProduct()
+  }, [])
 
   return (
     <Container>
@@ -121,13 +121,14 @@ const LandingPage = () => {
             <div className={`${styled.select}`}>
               <select
                 className={`${styled["select-tag"]}`}
-                onChange={handleChangeCity}>
+                onChange={handleChangeCity}
+              >
                 {listCity.map((city, id) => {
                   return (
                     <option value={city.id} key={id}>
                       {city.lokasi}
                     </option>
-                  );
+                  )
                 })}
               </select>
             </div>
@@ -139,10 +140,12 @@ const LandingPage = () => {
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1200 120"
-            preserveAspectRatio="none">
+            preserveAspectRatio="none"
+          >
             <path
               d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              className={styled["shape-fill"]}></path>
+              className={styled["shape-fill"]}
+            ></path>
           </svg>
         </div>
       </HeroWrapper>
@@ -150,7 +153,6 @@ const LandingPage = () => {
         <ProductTitle>List Product</ProductTitle>
         <ProductList>
           {listProduct.map((product, index) => {
-            console.log({ product });
             return (
               <ProductBox key={index}>
                 <ProductImageWrapper>
@@ -165,7 +167,7 @@ const LandingPage = () => {
                   <ProductDetailDesc>{product.description}</ProductDetailDesc>
                 </ProductDetail>
               </ProductBox>
-            );
+            )
           })}
         </ProductList>
         <Link href="/store" passHref>
@@ -173,7 +175,7 @@ const LandingPage = () => {
         </Link>
       </ProductWrapper>
     </Container>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
