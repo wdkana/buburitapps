92ec5372d883bf24494953f63ec9e2336c1cd5af
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2022-04-17 06:14:24 +0700

    create feature component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,13 @@
-import AboutComponent from "../src/components/About/About";
 import Header from "../src/components/Header/Header";
+import AboutComponent from "../src/components/About/About";
+import FooterComponent from "../src/components/Footer/Footer";
 
 const About = () => {
   return (
     <>
       <Header />
       <AboutComponent />
+      <FooterComponent />
     </>
   );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
-import Head from "next/head";
 import Header from "../src/components/Header/Header";
 import LandingPage from "../src/components/LandingPage/LandingPage";
+import FooterComponent from "../src/components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header />
       <LandingPage />
+      <FooterComponent />
     </>
   );
 };
