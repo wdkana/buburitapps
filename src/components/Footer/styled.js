a7ce22ebc3390de4d7ddcd990ad3fd2a46b3dcf2
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2022-04-17 06:19:45 +0700

    fix responsive footer

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -4,6 +4,10 @@ export const Footer = styled.footer`
   padding: 5em 10em 3em 10em;
   background-color: #fff;
   z-index: 1;
+
+  @media (max-width: 768px) {
+    padding: 5em 5em 3em 5em;
+  }
 `;
 
 export const Wrapper = styled.div`
